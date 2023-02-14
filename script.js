7c0b4a01807875af2a1166b491984325f30aec0e
Giovanni El Baruqui <98186906+giovannielbaruqui@users.noreply.github.com>
2023-02-14 13:24:27 -0300

    #

---
+++ script.js
@@ -0,0 +1,13 @@
+const primeiraLi = document.querySelector('li');
+const primeiraUl = document.querySelector('ul');
+const linkInterno = document.querySelector('[href^="#"]');
+const animaisImg = document.querySelectorAll('.animais img');
+
+
+const gridSectionHtml = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+primeiraUl.classList.add('grid-section');
+
+console.log(gridSectionHtml);
+console.log(gridSectionNode);
